fc32c1ad862e3569621ee9ceb13de20283545bc3
Herdt <herdt.kai@gmail.com>
2021-03-01 07:52:18 +0300

    first commit

---
+++ app/Http/Livewire/Admin/AdiminDashboardComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdiminDashboardComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.adimin-dashboard-component')->layout('layouts.base');
+    }
+}
---
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,2 @@
+
+    <h1>User/customer dashboard </h1>
